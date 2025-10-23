687bceba425e96601373019a1f69346e6b6ce569
José Chonay <jfchonay@gmail.com>
2025-10-23 16:06:59 +0200

    get start and end coordinates

--- get_coordinates.py
+++ get_coordinates.py
@@ -1,12 +1,16 @@
 from pathlib import Path
+from turtledemo.penrose import start
+
 import pandas as pd
 import json
 
 # Base folder
-DATA_DIR = Path("/Users/josechonay/Documents/BIOSTAT_data")
+DATA_DIR = Path(r"P:\BIOSTAT\raw_data")
 
 # Target file names (without extensions)
 targets = ["Markers-events", "rigidBody"]
+start_list = []
+end_list = []
 
 # Iterate over each subject folder
 for subj_dir in DATA_DIR.iterdir():
@@ -40,8 +44,28 @@ for subj_dir in DATA_DIR.iterdir():
         events = loaded["Markers-events"]["csv"]
         coords = loaded["rigidBody"]["csv"]
 
+        # We just want the x, y, z rigid columns
+        coord_cols = meta['columns'][4:7]
+        coords_xyz = coords[coord_cols]
+
         # Example operation: extract coordinates between two marker timestamps
-        start_time = events.loc[events['value'] == 'Station scene  starting free choice phase', 'sample'].values
-        stop_time = events.loc[events['value'] == 'Station scene  user told operator why they chose this spot  ' \
+        start_sample = events.loc[events['value'] == 'Station scene  starting free choice phase', 'sample'].values
+        stop_sample = events.loc[events['value'] == 'Station scene  user told operator why they chose this spot  ' \
                                                   'showing waypoint to assessment point', 'sample'].values
+
+        # Extract the coordinates for every event
+        start_coord = coords_xyz.loc[start_sample]
+        end_coord   = coords_xyz.loc[stop_sample]
+
+        start_list.append(start_coord.values)
+        end_list.append(end_coord.values)
+# Deleting any elements that show up empty
+clean_start = [i_s[0] for i_s in start_list if len(i_s) > 0]
+clean_end = [i_e[0] for i_e in end_list if len(i_e) > 0]
+out_dir = r'P:\BIOSTAT\nudging\coordinates'
+start_df = pd.DataFrame(clean_start, columns=coord_cols)
+end_df   = pd.DataFrame(clean_end, columns=coord_cols)
+
+start_df.to_csv(out_dir + r"\start_coordinates.csv", index=False)
+end_df.to_csv(out_dir + r"\end_coordinates.csv", index=False)
 
