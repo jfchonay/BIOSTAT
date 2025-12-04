5bfe09768bc1619f3c6b402b7bd1f55b1acbb102
José Chonay <jfchonay@gmail.com>
2025-12-04 12:42:49 +0100

    eda cutting

--- eda_events.py
+++ eda_events.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 # Root folder that contains one subfolder per subject and Output folder
 ROOT_DIR = Path(r"P:\BIOSTAT\resampled")
-OUT_DIR = Path("/Users/josechonay/Documents/stress_physio")
+OUT_DIR = Path(r"P:\BIOSTAT\stress_section")
 
 # Filenames inside each subject folder (change if yours differ)
 MANIFEST_FILENAME = "manifest.json"
@@ -14,12 +14,11 @@ EVENTS_FILENAME = "Markers-events.csv"
 GSR_FILENAME = "GSR.csv"
 
 # Events names
-BASE_EVENT = "Station scene  end baseline  showing waypoint to answer form pos"
-STRS_EVENT = "Station scene  showing waypoint to recovery phase"
-REC_EVENT  = "Station scene  end recovery phase  showing waypoint to debrief recovery"
+FIRST_EVENT = "Playing voice over  TutorialStartBaseline"
+LAST_EVENT = "Station scene  end recovery phase  showing waypoint to debrief recovery"
 
 # Parameters of the epoch
-s_b = 3 * 100
+s_b = 1 * 100
 s_a = 1 * 100
 
 greenery_true = []
@@ -40,18 +39,17 @@ for subject_dir in ROOT_DIR.iterdir():
         events_df[col] = pd.to_numeric(events_df[col], errors="coerce")
     gsr_csv_path = subject_dir / GSR_FILENAME
     gsr_df = pd.read_csv(gsr_csv_path)
+    # Check if the events exist in the file
+    if not any(events_df.index[events_df["value"] == FIRST_EVENT]) and any(events_df.index[events_df["value"] == LAST_EVENT]) :
+        print (f"[WARN] No events in {subject_dir}, skipping.")
+        continue
     # Using the name of the events get the sample in which every event is found
-    base_idx = events_df["sample"][events_df.index[events_df["value"] == BASE_EVENT][0]]
-    strs_idx = events_df["sample"][events_df.index[events_df["value"] == STRS_EVENT][0]]
-    rec_idx  = events_df["sample"][events_df.index[events_df["value"] == REC_EVENT][0]]
+    first_idx = events_df["sample"][events_df.index[events_df["value"] == FIRST_EVENT][0]]
+    last_idx = events_df["sample"][events_df.index[events_df["value"] == LAST_EVENT][0]]
     # Use the sample to cut the section of data from the GSR
-    gsr_base = np.array(gsr_df["CH1"][base_idx-s_b:base_idx+s_a])
-    gsr_strs = np.array(gsr_df["CH1"][strs_idx-s_b:strs_idx+s_a])
-    gsr_rec = np.array(gsr_df["CH1"][rec_idx-s_b:rec_idx+s_a])
+    gsr_event = np.array(gsr_df["CH1"][first_idx-s_b:last_idx+s_a])
     # Invert the data and get the mean
-    base = np.mean([-b for b in gsr_base])
-    stress = np.mean([-s for s in gsr_strs])
-    recover = np.mean([-r for r in gsr_rec])
+    gsr = [-g for g in gsr_event]
     # save accordingly to the group they belong
     manifest_path = subject_dir / MANIFEST_FILENAME
     if not manifest_path.exists():
@@ -63,9 +61,9 @@ for subject_dir in ROOT_DIR.iterdir():
     # According to group save in desired list
     greenery = manifest.get("greenery")
     if greenery == "1":
-        greenery_true.append([base, stress, recover])
+        greenery_true.append(gsr)
     elif greenery == "0":
-        greenery_false.append([base, stress, recover])
+        greenery_false.append(gsr)
 
 # Convert lists to numpy arrays for easy statistics
 green = np.array(greenery_true)   # shape = (n, 3)
